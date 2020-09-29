ac9fcb843ed8972bfd340b47a608ec72569cc1cc
yashkotadia <yashkotadia@gmail.com>
2020-09-28 21:23:40 -0700

    relu bug fix

--- dyresnet.py
+++ dyresnet.py
@@ -110,7 +110,7 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
